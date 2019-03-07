23fd4f028c8bb51950496dfe7ae31dc6dd7d9a36
曲广凡 <guangfan.qu@alibaba-inc.com>
2019-03-07 15:17:12 +0800

    Modified the example main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,11 +1,11 @@
 module "instance" {
   source = "../"
-  password = "MyTest@1234"
+  password = "Your password"
   data_node_spec = "elasticsearch.sn2ne.large"
   data_node_amount = "2"
   data_node_disk_size = "20"
   data_node_disk_type = "cloud_ssd"
   es_version = "5.5.3_with_X-Pack"
-  vswitch_id = "vsw-uf6hser75qrlib5idurat"
+  vswitch_id = "vswitch id"
   master_node_spec = "elasticsearch.sn2ne.large"
 }
