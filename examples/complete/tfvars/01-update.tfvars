c749f134dfacf80c905f051c37e92b71aa46dab6
shanye <shanye.xzq@alibaba-inc.com>
2024-05-30 17:42:39 +0800

    module/alicloud_elasticsearch_instance: support attributes kibana_node_spec, data_node_disk_performance_level; examples/complete: improve the examples

--- examples/complete/tfvars/01-update.tfvars
+++ examples/complete/tfvars/01-update.tfvars
@@ -1,11 +1,9 @@
 #alicloud_elasticsearch_instance
 period              = 2
 data_node_amount    = 3
-data_node_spec      = "elasticsearch.sn2ne.large"
 data_node_disk_size = 30
 data_node_disk_type = "cloud_ssd"
 password            = "YourPassword123!Update"
 private_whitelist   = ["172.16.0.0/21"]
 kibana_whitelist    = ["172.16.0.0/21"]
-master_node_spec    = "elasticsearch.sn2ne.xlarge"
 description         = "update-tf-description"
--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
 resource "alicloud_elasticsearch_instance" "instance" {
   count = var.number_of_instance
 
-  instance_charge_type = var.instance_charge_type
-  period               = var.period
-  data_node_amount     = var.data_node_amount
-  data_node_spec       = var.data_node_spec
-  data_node_disk_size  = var.data_node_disk_size
-  data_node_disk_type  = var.data_node_disk_type
-  vswitch_id           = var.vswitch_id
-  password             = var.password
-  version              = var.es_version
-  private_whitelist    = var.private_whitelist
-  kibana_whitelist     = var.kibana_whitelist
-  master_node_spec     = var.master_node_spec
-  description          = var.description
+  instance_charge_type             = var.instance_charge_type
+  period                           = var.period
+  data_node_amount                 = var.data_node_amount
+  data_node_spec                   = var.data_node_spec
+  data_node_disk_size              = var.data_node_disk_size
+  data_node_disk_type              = var.data_node_disk_type
+  vswitch_id                       = var.vswitch_id
+  password                         = var.password
+  version                          = var.es_version
+  private_whitelist                = var.private_whitelist
+  kibana_whitelist                 = var.kibana_whitelist
+  master_node_spec                 = var.master_node_spec
+  description                      = var.description
+  data_node_disk_performance_level = var.data_node_disk_performance_level
+  kibana_node_spec                 = var.kibana_node_spec
 }
